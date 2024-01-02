a866e87993db7c331529f831abb9ad4927ae90b8
10thcode <attabadukwabena@gmail.com>
2024-01-02 23:06:53 +0000

    Update 1-block-scoped.js
    Disable jslint unused variable check.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     var task = true;
     var task2 = false;
   }
 
-  task = false;
-  task2 = true;
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
