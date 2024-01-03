c1d946dd87d61a5e344ac6dd04be00ea3667ad80
10thcode <attabadukwabena@gmail.com>
2024-01-03 06:13:03 +0000

    Update 1-block-scoped.js
    fix some eslint issues

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-  if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
-  }
-  task = false;
-  task2 = true;
+  const task = false;
+  const task2 = true;
+
   return [task, task2];
 }
