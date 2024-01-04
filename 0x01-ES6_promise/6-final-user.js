29ee7e6a14c00b99f725849d5ab0d9c97d9fd98f
10thcode <attabadukwabena@gmail.com>
2024-01-04 05:32:02 +0000

    Update 6-final-user.js
    return promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const p1 = signUpUser(firstName, lastName);
-  const p2 = uploadPhoto(fileName);
-  return Promise.allSettled([p1, p2]).then((res) => {
-    return (res);
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
