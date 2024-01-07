862f653185ade42383079e02f6bd9611de24143a
10thcode <attabadukwabena@gmail.com>
2024-01-07 22:43:50 +0000

    Add 8-hbtn_class.hs

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,46 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new Error('Size must be a number');
+    }
+
+    if (typeof location !== 'string') {
+      throw new Error('Location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Size must be a number');
+    }
+
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Location must be a string');
+    }
+
+    this._size = value;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+
+  toString() {
+    return this.location;
+  }
+}
