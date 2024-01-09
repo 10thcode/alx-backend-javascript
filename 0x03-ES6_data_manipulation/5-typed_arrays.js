ccb8c55e0304f7ad1bd427332e7dd07cb1fbeccb
10thcode <attabadukwabena@gmail.com>
2024-01-09 22:58:38 +0000

    Add 5-typed_arrays.js

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const arr = new Int8Array(buffer);
+
+  arr[position] = value;
+
+  return new DataView(buffer);
+}
