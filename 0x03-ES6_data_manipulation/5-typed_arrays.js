4602efc4ebb392555d9e1188b752c0c16fd20090
10thcode <attabadukwabena@gmail.com>
2024-01-09 23:01:03 +0000

    Update 5-typed_arrays.js
    Throw an error message if adding the value is not possible

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,8 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const arr = new Int8Array(buffer);
 
-  arr[position] = value;
+  if (position < length) arr[position] = value;
+  else throw new Error('Position outside range');
 
   return new DataView(buffer);
 }
