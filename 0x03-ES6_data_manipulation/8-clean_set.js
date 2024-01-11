c5f6895aebebfce728b119159947db0a56053c7a
10thcode <attabadukwabena@gmail.com>
2024-01-11 14:14:07 +0000

    Update 8-clean_set.js
    fix some bugs

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startcleanSet) {
-  if (startcleanSet === "") return "";
+  if (startcleanSet === '') return '';
 
   const str = [];
 
   for (const item of set.values()) {
-    if (item.startsWith(startcleanSet)) str.push(item.slice(startcleanSet.length));
+    if (item && item.startsWith(startcleanSet)) str.push(item.slice(startcleanSet.length));
   }
 
   return str.join('-');
