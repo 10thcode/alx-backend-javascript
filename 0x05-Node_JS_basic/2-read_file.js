ddd201dfb09a55cd36ebc1e393fe284f1201dfa0
10thcode <attabadukwabena@gmail.com>
2024-03-01 11:56:53 +0000

    Add 2-read-file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  const data = [];
+  let raw = '';
+
+  try {
+    raw = fs.readFileSync(path, 'utf-8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  raw.split('\n').filter((arr) => arr.length > 0).slice(1).forEach((row) => {
+    data.push(row.split(','));
+  });
+
+  const students = data.length;
+  const fields = {};
+
+  data.forEach((arr) => {
+    const field = arr[3];
+    if (fields[field] === undefined) {
+      fields[field] = [];
+      data.filter((row) => row[3] === field).forEach((x) => {
+        fields[field].push(x[0]);
+      });
+    }
+  });
+
+  console.log(`Number of students: ${students}`);
+  for (const field in fields) {
+    if (field) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  }
+};
