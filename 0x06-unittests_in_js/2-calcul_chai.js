4f222a7a387a91192ef1fbe803b87edf7ed464c7
10thcode <attabadukwabena@gmail.com>
2024-03-01 22:49:38 +0000

    Add 2-calcul_chai.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,18 @@
+function calculateNumber (type, a, b) {
+  const ra = Math.round(a);
+  const rb = Math.round(b);
+
+  if (type === "SUM") {
+    return ra + rb;
+  } else if (type === "SUBTRACT") {
+    return ra - rb;
+  } else if (type === "DIVIDE") {
+    if (rb === 0) {
+      return "Error";
+    } else {
+      return Math.round(a) / Math.round(b);
+    }
+  }
+}
+
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const calculateNumber = require('./2-calcul_chai.js');
-const expect = require('chai');
+const { expect } = require('chai');
 
 describe("testSum", function () {
   it("test with real numbers", function () {
@@ -7,38 +7,38 @@ describe("testSum", function () {
   });
 
   it("test with floating numbers", function () {
-    expect(6).to.equal(calculateNumber('SUM', 1.4, 4.5));
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
 
   it("test with both real and floating numbers", function () {
-    expect(6).to.equal(calculateNumber('SUM', 1, 4.5));
+    expect(calculateNumber('SUM', 1, 4.5)).to.equal(6);
   });
 });
 
 describe("testSubtract", function () {
   it("test with real numbers", function () {
-    expect(0).to.equal(calculateNumber('SUBTRACT', 3, 3));
+    expect(calculateNumber('SUBTRACT', 3, 3)).to.equal(0);
   });
 
   it("test with floating numbers", function () {
-    expect(-4).to.equal(calculateNumber('SUBTRACT', 1.4, 4.5));
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
 
   it("test with both real and floating numbers", function () {
-    expect(-4).to.equal(calculateNumber('SUBTRACT', 1, 4.5));
+    expect(calculateNumber('SUBTRACT', 1, 4.5)).to.equal(-4);
   });
 });
 
 describe("testDivide", function () {
   it("test with real numbers", function () {
-    expect(1).to.equal(calculateNumber('DIVIDE', 3, 3));
+    expect(calculateNumber('DIVIDE', 3, 3)).to.equal(1);
   });
 
   it("test with floating numbers", function () {
-    expect(3).to.equal(calculateNumber('DIVIDE', 5.5, 1.6));
+    expect(calculateNumber('DIVIDE', 5.5, 1.6)).to.equal(3);
   });
 
   it("test with 0", function () {
-    expect("Error").to.equal(calculateNumber('DIVIDE', 4, 0));
+    expect(calculateNumber('DIVIDE', 4, 0)).to.equal("Error");
   });
 });
